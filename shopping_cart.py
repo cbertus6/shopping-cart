9382a5688caf29b858c3cbbfd67acadd1d5aaff3
cbertus6 <cb4705@stern.nyu.edu>
2020-06-02 14:09:23 -0500

    Assemble running total price

--- shopping_cart.py
+++ shopping_cart.py
@@ -41,6 +41,8 @@ def to_usd(my_price):
 # INFO CAPUTRE / INPUT
 #
 
+total_price = 0
+
 while True:
     selected_id = input("Please input a product identifier: ") #> "9" (string)
     if selected_id == "DONE":
@@ -48,17 +50,16 @@ while True:
     else:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
         print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
-
-
-
-
 
 
 
 #
 # INFO DISPLAY / OUTPUT
 #
+
+print("TOTAL PRICE: " + str(total_price))
 
 
 # A grocery store name of your choice
