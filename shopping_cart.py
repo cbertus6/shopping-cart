ab166975be9effcd6788b5b2e4fdb5f12de77ff7
cbertus6 <cb4705@stern.nyu.edu>
2020-06-02 14:05:19 -0500

    Iteratively prompt user for inputs and handle DONE

--- shopping_cart.py
+++ shopping_cart.py
@@ -41,10 +41,19 @@ def to_usd(my_price):
 # INFO CAPUTRE / INPUT
 #
 
-selected_id = input("Please input a product identifier: ") #> "9" (string)
-matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-matching_product = matching_products[0]
-print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+while True:
+    selected_id = input("Please input a product identifier: ") #> "9" (string)
+    if selected_id == "DONE":
+        break
+    else:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+
+
+
+
 
 
 #
